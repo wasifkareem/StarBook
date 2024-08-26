c1d656511cc5b040893fcd33c004ca0981fed68c
wasif <wasifkareem2762@gmail.com>
2024-08-26 23:02:55 +0530

    script added

--- client/src/components/Wall.jsx
+++ client/src/components/Wall.jsx
@@ -4,11 +4,11 @@ import { useState } from "react";
 
 const Wall = ({ publicTestimonials, setWallPageToggle, spaceId }) => {
   const [theme, setTheme] = useState(false);
-  const code = ` <iframe id='starbook-id=${spaceId}' src="${window.location.origin}/embed/${spaceId}?
-   dark=${theme}" frameborder="0" scrolling="no" width="100%"></iframe>
-  <script src="https://cdn.jsdelivr.net/npm/@iframe-resizer/parent"></script>
+  const code = `  <iframe id="starbook-${spaceId}" src="https://starbook2762.vercel.app/embed/${spaceId}?dark=true" frameborder="0" scrolling="no" width="100%"></iframe>
+		<script src="https://cdn.jsdelivr.net/npm/@iframe-resizer/parent"></script>
 		<script>
-			iframeResize({license: "GPLv3",log: true, checkOrigin: false,}, '#starbook-id=${spaceId}');
+			iframeResize({license: "GPLv3",log: true, checkOrigin: false,}, '#starbook-${spaceId}');
+		</script>
 `;
   const handleClick = () => {
     setTheme(!theme);
