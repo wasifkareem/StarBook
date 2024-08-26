8043bcf58310a34385db0afd2db2b33d590a118c
wasif <wasifkareem2762@gmail.com>
2024-08-26 23:05:40 +0530

    changes in script

--- client/src/components/Wall.jsx
+++ client/src/components/Wall.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 const Wall = ({ publicTestimonials, setWallPageToggle, spaceId }) => {
   const [theme, setTheme] = useState(false);
-  const code = `  <iframe id="starbook-${spaceId}" src="https://starbook2762.vercel.app/embed/${spaceId}?dark=true" frameborder="0" scrolling="no" width="100%"></iframe>
+  const code = `  <iframe id="starbook-${spaceId}" src="https://starbook2762.vercel.app/embed/${spaceId}?dark=${theme}" frameborder="0" scrolling="no" width="100%"></iframe>
 		<script src="https://cdn.jsdelivr.net/npm/@iframe-resizer/parent"></script>
 		<script>
 			iframeResize({license: "GPLv3",log: true, checkOrigin: false,}, '#starbook-${spaceId}');
