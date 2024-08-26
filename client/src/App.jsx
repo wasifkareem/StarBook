1c59a1e1d6c14dcbf20051e62e31a01a94e234ad
wasif <wasifkareem2762@gmail.com>
2024-08-26 21:56:52 +0530

    resizer change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import ReviewPage from "./pages/ReviewPage";
 import Embed from "./pages/Embed";
 import "react-loading-skeleton/dist/skeleton.css";
 import "react-tooltip/dist/react-tooltip.css";
+import "@iframe-resizer/child";
 import LandingPage from "./pages/LandingPage";
 
 function App() {}
