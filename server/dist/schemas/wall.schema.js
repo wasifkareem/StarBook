78d24fbdbeaa66bada266ed38e76cb008abea70e
wasif kareem <wasifkareem2762@gmail.com>
2025-07-25 03:31:12 +0530

    :fix:ts compiler

--- server/dist/schemas/wall.schema.js
+++ server/dist/schemas/wall.schema.js
@@ -1,8 +1,5 @@
 import z from "zod";
 import { deleteTestimonialSchema } from "./testimonial.schema";
-
 export const updateWallQuerySchema = deleteTestimonialSchema.extend({
-WOF: z.boolean()
-})
-
-export type updateWallQuery = z.infer<typeof updateWallQuerySchema>
+    WOF: z.boolean()
+});
